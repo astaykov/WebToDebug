baedd5c61e797d84977fe6598a03be655f43a3d2
Anton Staykov <astaykov@gmail.com>
2015-07-10 12:43:22 +0000

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebToDebug.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = System.Configuration.ConfigurationManager.AppSettings["T"];
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            System.Diagnostics.Trace.TraceWarning("Application is about to crash!");
+            throw new ApplicationException("I just crashed");
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
